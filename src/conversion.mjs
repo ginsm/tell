461c147492a04f63eaec79208901e8b209ba18dc
ginsm <mattg.0609@gmail.com>
2020-12-04 06:00:49 -0800

    Reference commit #5fb811f
    This is what happens when you decide to push changes while
    being tired haha.. oops.

--- src/conversion.mjs
+++ src/conversion.mjs
@@ -1,26 +1,40 @@
-import { curry, assignMethodAlias } from './util/conversion.util.mjs';
+import { curry, assignMethodAliases } from './util/conversion.util.mjs';
 
+// TODO - Methods like score should *only* accept embedded objects
+// and throw an error out otherwise. I should probably make another
+// curry, i.e. curry.embedded
 const methods = {
-  //  Generic Text Formatting
-  text: curry.generic({ prop: 'text' }),
-  font: curry.generic({ prop: 'font', _default: 'minecraft:default' }),
-  color: curry.generic({ prop: 'color', _default: 'white' }),
+  // Generic Text Formatting
+  font: curry.generic({ prop: 'font', fallback: 'minecraft:default' }),
+  color: curry.generic({ prop: 'color', fallback: 'white' }),
   bold: curry.generic({ prop: 'bold' }),
   italic: curry.generic({ prop: 'italic' }),
   underlined: curry.generic({ prop: 'underlined' }),
   strikethrough: curry.generic({ prop: 'strikethrough' }),
   obfuscated: curry.generic({ prop: 'obfuscated' }),
 
-
-  //  Content Handlers
+  // Content Methods
+  text: curry.generic({ prop: 'text' }),
   value: curry.generic({ prop: 'value' }),
   contents: curry.generic({ prop: 'contents' }),
+  extra: curry.generic({ prop: 'extra' }),
+  translate: curry.generic({ prop: 'translate' }),
+  with: curry.generic({ prop: 'with' }),
+  score: curry.generic({ prop: 'score' }),
+  objective: curry.generic({ prop: 'objective' }),
+  name: curry.generic({ prop: 'name' }),
+  selector: curry.generic({ prop: 'selector' }),
+  keybind: curry.generic({ prop: 'keybind' }),
+  nbt: curry.generic({ prop: 'nbt' }),
+  block: curry.generate({ prop: 'block' }),
+  entity: curry.genreate({ prop: 'entity' }),
+  storage: curry.generate({ prop: 'storage' }),
+  extra: curry.generate({ prop: 'extra' }),
 
-
-  //  Event Handlers
+  // Shift Click Event
+  insertion: curry.generic({ prop: 'insertion' }), // appends text to user's chatbox
 
   // Click Events
-  insertion: curry.generic({ prop: 'insertion' }), // shift + click (appends to user chat)
   copytoclipboard: curry.event({ type: 'click', action: 'copy_to_clipboard', ignoreEmpty: true }),
   openurl: curry.event({ type: 'click', action: 'open_url', ignoreEmpty: true }),
   runcmd: curry.event({ type: 'click', action: 'run_command', ignoreEmpty: true }),
@@ -33,7 +47,7 @@ const methods = {
   hoverentity: curry.event({ type: 'hover', action: 'show_entity' }),
 };
 
-assignMethodAlias(methods, {
+assignMethodAliases(methods, {
   t: 'text',
   f: 'font',
   c: 'color',
@@ -43,6 +57,7 @@ assignMethodAlias(methods, {
   s: 'strikethrough',
   o: 'obfuscated',
   v: 'value',
+  key: 'keybind',
   insert: 'insertion',
   copy: 'copytoclipboard',
   url: 'openurl',
